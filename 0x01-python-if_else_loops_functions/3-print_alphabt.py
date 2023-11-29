e8cb9ffc2c53361976a55eafab95eee6bb56021a
ysmurtala <yaqoobmurry@gmail.com>
2023-11-28 23:35:17 -0800

    Alphabets in lowercase less q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for a in range(97, 123):
-    if (i == 101) or (i == 113):
+    if (a == 101) or (a == 113):
         continue
-    print(chr(i).format(), end="")
-
+    print(chr(a).format(), end="")
